9d182b63998db81ced1e04bcd66f317cc2207f11
Ilker Yildirim <ITSIlkerSE@gmail.com>
2022-09-22 21:18:15 +0200

    Rick and Morty api implementation

--- src/components/CharacterGallery.tsx
+++ src/components/CharacterGallery.tsx
@@ -1,6 +1,8 @@
 import "./CharacterGallery.css"
 import {Character} from "../model/Character";
 import CharacterCard from "./CharacterCard";
+import { useState } from "react";
+
 
 type CharacterGalleryProps = {
 
@@ -10,17 +12,27 @@ type CharacterGalleryProps = {
 
 export default function CharacterGallery(props: CharacterGalleryProps) {
 
+    const [filterText, setFilterText] = useState( "");
+
+    const result = props.characters.filter((char) => char.name.toLocaleLowerCase().includes(filterText.toLocaleLowerCase()));
 
     return (
-        <div className="cards">
+           <div>
+            <input className={"input"}
+                onChange={(event) =>
+                setFilterText(event.target.value)}/>
+
+            <p>Typed in: {filterText}</p>
+            <div className="cards">
+                {result.map((char) =>
+                    <div className={"card"}>
+                        <CharacterCard charVal={char}/>
+                    </div>)}
 
 
-            {props.characters.map((char) => <div className="card"><CharacterCard charVal={char}/></div>)}
-
+            </div>
 
         </div>
-
-
     )
 
 }
